5942a926f729b4bf4effc303e43f16d980fa42be
benzy78 <westvillage815@gmail.com>
2025-01-18 02:09:40 +0900

    SPのコーディング完了

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ $(document).ready(function () {
     $(this).toggleClass("active");
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".header_nav");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
+
