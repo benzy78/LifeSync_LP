3652bd7ec466019ecae703bc61a99eaf94c7192d
benzy78 <westvillage815@gmail.com>
2025-01-18 12:17:15 +0900

    ハンバーメニューの挙動修正

--- js/script.js
+++ js/script.js
@@ -11,9 +11,16 @@ $(document).ready(function () {
 document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.querySelector(".menu-toggle");
   const navLinks = document.querySelector(".header_nav");
+  const navItems = document.querySelectorAll(".nav_links a"); 
 
   menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+  });
+
+  navItems.forEach((link) => {
+    link.addEventListener("click", () => {
+      navLinks.classList.remove("active"); // メニューを閉じる
+    });
   });
 });
 
